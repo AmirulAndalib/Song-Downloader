0bc1302b75dae53bbf738b36c1f9b6d44f11bc95
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2022-12-07 05:47:13 +0530

    Update config.py
    Signed-off-by: Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config(object):
+    APP_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DATABASE = os.environ.get("DB_URI")
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
     SUPPORT = os.environ.get("SUPPORT")
